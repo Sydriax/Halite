7be1128da5006a06f63bc187c55add5336734c5c
Michael Truell <michael_truell@horacemann.org>
2016-08-11 16:06:53 -0400

    Common website header

---
+++ website/includes/header.php
@@ -0,0 +1,5 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="UzLAOvN92N2iaw_7HcFXSOc_M-WIe3KFXaozuaNsZo4" />
+<meta name="google-signin-client_id" content="876771541734-tok428u2vimm84fl4tcshvs3ufrbsngi.apps.googleusercontent.com">
