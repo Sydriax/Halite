25b51f057c3526166d6e6dac7753c494d35af4b2
George Moschovitis <george.moschovitis@gmail.com>
2016-12-18 18:47:33 +0200

    Provide RandomBot and MyBot
    The directory is also better organized now.

--- airesources/Swift/runGame.sh
+++ airesources/Swift/runGame.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-swiftc -O Model.swift Networking.swift main.swift -o MyBot
-./halite -d "30 30" "./MyBot" "./MyBot"
+swiftc -O ./Sources/Halite/Model.swift ./Sources/Halite/Networking.swift Sources/MyBot/main.swift -o MyBot
+swiftc -O ./Sources/Halite/Model.swift ./Sources/Halite/Networking.swift Sources/RandomBot/main.swift -o RandomBot
+./halite -d "30 30" "./MyBot" "./RandomBot"
